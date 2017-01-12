56d457075736c38ad3d1c0016e36d4a542084e11
Florian Begusch <>
2017-01-12 10:58:22 +0100

    Fix argument input; fix listing of files; add basic confirmation for string found in file

--- grep_redone/__main__.py
+++ grep_redone/__main__.py
@@ -3,19 +3,27 @@ import sys
 
 from grep import grep as grep_
 def main(args=None):
-    if sys.argv.__len__() < 2:
-        print "grep_redone: No string to search for. Exiting."
-    elif sys.argv[1] == '-h':
-        pass
-    elif sys.argv[1].__class__ == str:
-        string_to_search_for = sys.argv[1]
-        currentdir = os.path.abspath(os.path.curdir)
-        grep_.Searcher(currentdir, string_to_search_for)
+    
+    string_to_search_for = ""
+    is_to_search = False
+    currentdir = os.path.abspath(os.path.curdir)
+    
+    if sys.argv.__len__() == 2:
         
-    else:
-        print ("""grep_redone: unrecognized option '""" 
-            + sys.argv[1] + "\'\ngrep_redone: Use the"
-            + " -h option for usage information.")
+        if sys.argv[1] == '-h':
+            pass
+        
+        elif sys.argv[1] and not sys.argv[1].startswith('-'):
+            is_to_search = True
+            string_to_search_for = sys.argv[1]
+            
+        else:
+            print ("""grep_redone: unrecognized option '""" 
+                   + "\'\ngrep_redone: Use the"
+                   + " -h option for usage information.")
+    
+    if is_to_search:        
+        grep_.Searcher(currentdir, string_to_search_for)
 
 if __name__ == "__main__":
     main()
--- grep_redone/grep/grep.py
+++ grep_redone/grep/grep.py
@@ -1,7 +1,36 @@
+import os
+
 class Searcher(object):
-    
+    """Searches files in dirs for specified string."""
     def __init__(self, currentdir, string_to_search_for):
         self.currentdir = currentdir
         self.string_to_search_for = string_to_search_for
-        print self.string_to_search_for
+        
+        self.get_files_in_currentdir()
     
+    def search_files_in_dir_for_string(self, file_list):
+        for f in file_list:
+            self.search_file_for_string(f)
+    
+    def get_files_in_currentdir(self):
+        # TODO implement iterator file; iterate lines
+        file_list = []
+        for f in os.listdir(self.currentdir):
+            if not os.path.isdir(f):
+                file_list.append(f)
+    
+        if self.string_to_search_for:
+            self.search_files_in_dir_for_string(file_list)
+            
+        else:    
+            for f in file_list:
+                print f
+    
+    def search_file_for_string(self, f):
+        with open(f) as f:
+            for line in f:
+                if self.string_to_search_for in line:
+                    print 'now'
+    
+    def search_subdir(self):
+        pass
