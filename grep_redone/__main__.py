2d117f621ed0256811e993bf4ec0540cb44117a7
Florian Begusch <florian.begusch@edu.fh-joanneum.at>
2017-02-03 00:12:59 +0100

    Refactor grep; add basic recursive search

--- grep_redone/__main__.py
+++ grep_redone/__main__.py
@@ -2,28 +2,30 @@ import os
 import sys
 
 from grep import grep as grep_
+
+
 def main(args=None):
-    
     string_to_search_for = ""
-    is_to_search = False
-    currentdir = os.path.abspath(os.path.curdir)
-    
+
     if sys.argv.__len__() == 2:
-        
+
+        # TODO implement
         if sys.argv[1] == '-h':
             pass
-        
+
         elif sys.argv[1] and not sys.argv[1].startswith('-'):
-            is_to_search = True
+            is_searchable = True
             string_to_search_for = sys.argv[1]
-            
+            grep_.Searcher(os.path.abspath(os.path.curdir), string_to_search_for)
+
         else:
-            print ("""grep_redone: unrecognized option '""" 
+            print ("""grep_redone: unrecognized option '"""
                    + "\'\ngrep_redone: Use the"
                    + " -h option for usage information.")
-    
-    if is_to_search:        
-        grep_.Searcher(currentdir, string_to_search_for)
+
+    else:
+        grep_.Searcher(os.path.abspath(os.path.curdir), "")
+        # raise RuntimeError("No string to search for. Exiting.")
 
 if __name__ == "__main__":
     main()
--- grep_redone/grep/grep.py
+++ grep_redone/grep/grep.py
@@ -1,59 +1,78 @@
 import os
-from clint.textui import colored 
+from clint.textui import colored
+
 
 class Searcher(object):
     """Searches files in dirs for specified string."""
-    def __init__(self, currentdir, string_to_search_for):
-        self.currentdir = currentdir
+
+    isfirst = True
+
+    def __init__(self, caller_directory, string_to_search_for):
         self.string_to_search_for = string_to_search_for
-        
-        self.get_files_in_currentdir()
-    
-    def get_files_in_currentdir(self):
-        # TODO implement iterator file; iterate lines
+
+        self.dir_list = self.get_all_subdirectories_in_directory(caller_directory)
+
+        # TESTING
+        print self.dir_list
+
+        for i in range(self.dir_list.__len__() -1):
+            self.get_files_in_currentdir(self.dir_list[i])
+
+
+    def get_all_subdirectories_in_directory(self, curdir):
+        dir_list = [curdir]
+        for f in os.listdir(curdir):
+            if os.path.isdir(f):
+                dir_list.append(curdir + "/" + f)
+
+        return dir_list
+
+    # Build list with files in current directory
+    def get_files_in_currentdir(self, curdir):
         file_list = []
-        for f in os.listdir(self.currentdir):
+        print curdir + "right here"
+        for f in os.listdir(curdir):
             if not os.path.isdir(f):
                 file_list.append(f)
-    
+
+        # If there is a string to search --> do function call
         if self.string_to_search_for:
-            matched_file_dict = self.search_files_in_dir_for_string(file_list)
-            
+            matched_file_dict = self.search_files_for_string(file_list, curdir)
+
+            # If there were any matches --> do print
             if matched_file_dict:
                 self.print_nicely(matched_file_dict)
-            
-        else:    
+
+        # stub
+        else:
             for f in file_list:
                 print f
 
-
-    def search_files_in_dir_for_string(self, file_list):
+    def search_files_for_string(self, file_list, curdir):
         matched_file_dict = {}
-        
+
         for f in file_list:
-            matched_line_dict = self.search_file_for_string(f)
-            
+            matched_line_dict = self.search_file_for_string(f, curdir)
+
             if matched_line_dict:
                 matched_file_dict[f] = matched_line_dict
-        
+
         return matched_file_dict
 
-        
-    def search_file_for_string(self, f):
+    def search_file_for_string(self, file_path, curdir):
         matched_line_dict = {}
-        with open(f) as f:
-            for index, line in enumerate(f):
-                if self.string_to_search_for in line:
-                    matched_line_dict[index+1] = line
+        file = curdir + "/" + file_path
+        if not os.path.isdir(file):
+            with open(file) as f:
+                for index, line in enumerate(f):
+                    if self.string_to_search_for in line:
+                        matched_line_dict[index + 1] = line
 
         return matched_line_dict
-                    
-    
+
     def print_nicely(self, matched_file_dict):
+
         for key, value in matched_file_dict.iteritems():
-            
             for k, v in value.iteritems():
-                print (colored.magenta('./' + key + ':', True, False) +
-                    str(k) + ':' + v),
-                
-                
+                print (colored.magenta("./" + key + ':', True, False) +
+                       str(k) + ':' + v),
