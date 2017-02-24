5686cbc6d18551b14ac24ebdf3b9a826caa183b1
Florian Begusch <>
2017-02-24 09:23:26 +0100

    Add dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Grep Redone',
     version='0.1',
-    packages=['grep_redone'],
+    packages=['grep_redone', 'docopt'],
     entry_points={
         'console_scripts': [
             'grep_redone=grep_redone.__main__:main'
