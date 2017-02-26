a41f620b0930400256a270f3caf0bad1227aeffc
Florian Begusch <>
2017-02-26 20:24:38 +0100

    Fix waiting for files not on disk

--- grep_redone/grep/file_helper.py
+++ grep_redone/grep/file_helper.py
@@ -9,8 +9,11 @@ def get_all_files(caller_dir, is_recursive):
     file_paths = []
 
     for root, dirs, files in os.walk(caller_dir):
-        # Prepend root to every file path
-        file_paths.extend(['{0}/{1}'.format(root, file) for file in files])
+        for f in files:
+
+            if os.path.isfile(root + "/" + f):
+                # Prepend root to every file path
+                file_paths.extend(['{0}/{1}'.format(root, f)])
 
         if is_recursive is False:
             break
