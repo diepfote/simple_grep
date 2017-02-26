78d8d0422fbf9e419f61a7135717d9a59bec7c94
Florian Begusch <florian.begusch@edu.fh-joanneum.at>
2017-02-26 19:08:37 +0100

    Fix tests --use temporary dirs and files for testing

--- grep_redone/grep/file_helper.py
+++ grep_redone/grep/file_helper.py
@@ -3,14 +3,15 @@
 import os
 
 def get_all_files(caller_dir, is_recursive):
+
+    assert type(is_recursive) is bool, "Should always be a boolean."
+
     file_paths = []
 
     for root, dirs, files in os.walk(caller_dir):
-
         # Prepend root to every file path
         file_paths.extend(['{0}/{1}'.format(root, file) for file in files])
 
-        assert type(is_recursive) is bool, "Should always be a boolean."
         if is_recursive is False:
             break
 
