e4618832daaf3369c9d52ca16393fc52be4dd127
Florian Begusch <florian.begusch@edu.fh-joanneum.at>
2017-02-28 08:10:41 +0100

    Fix tests failing due to os specific file paths on windows

--- grep_redone/grep/file_helper.py
+++ grep_redone/grep/file_helper.py
@@ -5,17 +5,19 @@ import os
 def get_all_files(caller_dir, is_recursive):
 
     assert type(caller_dir) is str
-    assert type(is_recursive) is bool, "Should always be a boolean."
+    assert type(is_recursive) is bool
 
     file_paths = []
 
     for root, dirs, files in os.walk(caller_dir):
         for f in files:
 
+            # Environment specific file paths.
+            file_path = os.path.normpath('{0}/{1}'.format(root, f))
+
             # Check if it is an actual file on disk.
-            if os.path.isfile(root + "/" + f):
-                # Prepend root to every file path
-                file_paths.extend(['{0}/{1}'.format(root, f)])
+            if os.path.isfile(file_path):
+                file_paths.extend([file_path])
 
         if is_recursive is False:
             break
