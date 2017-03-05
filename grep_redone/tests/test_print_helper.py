892ba0cbf991fc416c3c42a9311971157ec3f418
Florian Begusch <florian.begusch@edu.fh-joanneum.at>
2017-03-05 17:24:35 +0100

    Fix failing test for is_f_binary_file

--- grep_redone/tests/test_print_helper.py
+++ grep_redone/tests/test_print_helper.py
@@ -18,8 +18,9 @@ def teardown_module():
 
 
 def test_print_matched_files_full_path():
-    test_dict = {'/home/flo/Untitled Document 1': {1: 'aware\n', 2: 'aware werwer\n'}}
-    test_output = ['\x1b[35m\x1b[22m' + os.path.normpath('/home/flo/Untitled Document 1') + ':\x1b[39m\x1b[22m2:\x1b[1;31maware\x1b[0m werwer', '\x1b[35m\x1b[22m' + os.path.normpath('/home/flo/Untitled Document 1') + ':\x1b[39m\x1b[22m1:\x1b[1;31maware\x1b[0m']
+    test_dict = {'/home/flo/Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
+    
+    test_output = ['\x1b[35m\x1b[22m' + os.path.normpath('/home/flo/Untitled Document') + ':\x1b[39m\x1b[22m2:\x1b[1;31maware\x1b[0m werwer', '\x1b[35m\x1b[22m' + os.path.normpath('/home/flo/Untitled Document') + ':\x1b[39m\x1b[22m1:\x1b[1;31maware\x1b[0m']
     output = print_helper.print_matched_files_full_path(test_dict, search_term='aware')
 
     assert output == test_output
@@ -27,15 +28,15 @@ def test_print_matched_files_full_path():
 
 def test_print_matched_files_relative_path():
     if platform.system() == 'Windows':
-        test_dict = {'./../../../../Untitled Document 1': {1: 'aware\n', 2: 'aware werwer\n'}}
+        test_dict = {'./../../../../Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
 
     elif platform.system() == 'Linux':
-        test_dict = {'/home/flo/Untitled Document 1': {1: 'aware\n', 2: 'aware werwer\n'}}
+        test_dict = {'/home/flo/Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
 
     else:
         raise ValueError('No system information found.')
 
-    test_output = ['\x1b[35m\x1b[22m' + os.path.normpath('../../../../Untitled Document 1') + ':\x1b[39m\x1b[22m2:\x1b[1;31maware\x1b[0m werwer', '\x1b[35m\x1b[22m' + os.path.normpath('../../../../Untitled Document 1') + ':\x1b[39m\x1b[22m1:\x1b[1;31maware\x1b[0m']
+    test_output = ['\x1b[35m\x1b[22m' + os.path.normpath('../../../../Untitled Document') + ':\x1b[39m\x1b[22m2:\x1b[1;31maware\x1b[0m werwer', '\x1b[35m\x1b[22m' + os.path.normpath('../../../../Untitled Document') + ':\x1b[39m\x1b[22m1:\x1b[1;31maware\x1b[0m']
     output = print_helper.print_matched_files_relative_path(test_dict, search_term='aware')
 
     assert output == test_output
