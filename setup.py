340bda7aa5d5e6220dea25dfe7b2248b2b85908b
Florian Begusch <>
2017-03-27 12:09:51 +0200

    Fix setup file after major restructuring

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Grep Redone',
     version='0.1',
-    packages=['grep_redone'],
+    packages=['grep'],
     install_requires=['docopt', 'clint', 'pytest'],
     entry_points={
         'console_scripts': [
