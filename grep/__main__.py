6fe1d8f0745b3b26a4e470ae2321c503234dc38b
Florian Begusch <>
2017-03-30 11:48:55 +0200

    Add functionality for pointing grep_redone at a specific file

--- grep/__main__.py
+++ grep/__main__.py
@@ -3,7 +3,7 @@
 grep_redone, version 0.9 
 Search files for a pattern or string, optionally do this recursively.
 
-usage: grep_redone [-rnfe] [SEARCH_TERM]
+usage: grep_redone [-rnpe] [SEARCH_TERM] [FILE_TO_SEARCH] 
 
 Arguments:
   SEARCH_TERM       The string to search for.
@@ -11,7 +11,7 @@ Arguments:
 Options:
   -h --help         Display this page.
   -r                Do a recursive search.
-  -f                Display full/absolute paths.
+  -p                Display full/absolute paths.
   -e                Use the search term as a regex pattern.
   -n                Display line numbers for matches.
 """
@@ -49,16 +49,27 @@ def main():
                     f.close()
                     assert type(directory) == str
 
-        else:  # an empty line means stdin has been closed
-            directory = os.path.abspath(os.path.curdir)
+        else:  # An empty line means stdin has been closed.
+
+            # Check if the specified path is a directory.
+            f = args['FILE_TO_SEARCH']
+            if f:
+                if os.path.isdir(os.path.abspath(f)):
+                    directory = f
+                    args['FILE_TO_SEARCH'] = ''
+
+            else:
+                directory = os.path.abspath(os.path.curdir)
 
         search_term = args['SEARCH_TERM'] if args['SEARCH_TERM'] else ''
+        specific_file = args['FILE_TO_SEARCH'] if args['FILE_TO_SEARCH'] else ''
 
         searcher = grep_.Searcher(
             caller_dir=directory,
             search_term=search_term,
+            specific_file=specific_file,
             is_recursive=args['-r'],
-            is_abs_path=args['-f'],
+            is_abs_path=args['-p'],
             is_regex_pattern=args['-e'],
             is_search_line_by_line=args['-n']
         )
