8edf873369526eba6cf39727f2e8a9407372dd86
Florian Begusch <florian.begusch@gmail.com>
2017-09-13 00:10:47 +0200

    Fix tests; fix pep8

--- tests/helper_for_tests.py
+++ tests/helper_for_tests.py
@@ -11,65 +11,64 @@ fd, temp_path = tempfile.mkstemp(dir=temp_dir, suffix='.txt', text=True)
 
 @pytest.fixture(scope='function')
 def with_f_read():
-	f = open(temp_path, 'r')
-	yield f
-	f.close()
+    f = open(temp_path, 'r')
+    yield f
+    f.close()
 
 
 @pytest.fixture(scope='function')
 def with_f_bread():
-	f = open(temp_path, 'rb')
-	yield f
-	f.close()
+    f = open(temp_path, 'rb')
+    yield f
+    f.close()
 
 
 @pytest.fixture(scope='function')
 def with_f_write():
-	f = open(temp_path, 'w')
-	yield f
-	f.close()
+    f = open(temp_path, 'w')
+    yield f
+    f.close()
 
 
 # TODO fix this temporary fix
 @pytest.fixture(scope='function')
 def with_f_bwrite():
-	if sys.version_info[0] < 3:
-		import io
-		f = io.open(temp_path, 'w', encoding='utf-8')
+    if sys.version_info[0] < 3:
+        import io
+        f = io.open(temp_path, 'w', encoding='utf-8')
 
-	else:
-		f = open(temp_path, 'w', encoding='utf-8')
+    else:
+        f = open(temp_path, 'w', encoding='utf-8')
 
-	f.write(u'ä')
-	f.seek(0)
-	yield f
-	f.close()
+    f.write(u'ä')
+    f.seek(0)
+    yield f
+    f.close()
 
 
 @pytest.fixture(scope='function')
 def with_restricted_file():
-	f = open(temp_path, 'r')
-	# Leading zero ensures int is treated as octal
-	os.chmod(f.name, 0o000)
-	yield temp_dir
+    f = open(temp_path, 'r')
+    # Leading zero ensures int is treated as octal
+    os.chmod(f.name, 0o000)
+    yield temp_dir
 
-	# Leading zero ensures int is treated as octal
-	os.chmod(f.name, 0o777)
+    # Leading zero ensures int is treated as octal
+    os.chmod(f.name, 0o777)
 
 
 # TODO make clean up less subtle
 @pytest.fixture(scope='function')
 def hotfix_delete_temp_dir(request):
-	# Delete directory after tests finished
-	def fin():
-		os.close(fd)
-		os.remove(temp_path)
-		os.removedirs(temp_dir)
+    # Delete directory after tests finished
+    def fin():
+        os.close(fd)
+        os.remove(temp_path)
+        os.removedirs(temp_dir)
 
-		# Check that file has been deleted
-		with pytest.raises(IOError):
-			open(temp_path, 'r')
+        # Check that file has been deleted
+        with pytest.raises(IOError):
+            open(temp_path, 'r')
 
-
-	# Run fin function after all tests have run
-	request.addfinalizer(fin)
+    # Run fin function after all tests have run
+    request.addfinalizer(fin)
--- tests/test_print_helper.py
+++ tests/test_print_helper.py
@@ -8,98 +8,101 @@ from tests.helper_for_tests import with_f_bwrite, hotfix_delete_temp_dir
 
 
 def test_generate_output_for_matched_files_full_path():
-	matched_items = {'/home/flo/Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
+    matched_items = {
+        '/home/flo/Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
 
-	test_output = [os.path.normpath('/home/flo/Untitled Document') + ':1:aware',
-	               os.path.normpath('/home/flo/Untitled Document') + ':2:aware werwer']
-	actual = print_helper.generate_output_for_matched_files_full_path(matched_items, search_term='aware',
-	                                                                  is_from_stdin=False, is_line_by_line=True)
+    test_output = ['\x1b[0;35m/home/flo/Untitled Document\x1b[0m\x1b[0;32m:\x1b[0m\x1b[0;32m1\x1b[0m\x1b[0;32m:\x1b[0maware',
+                   '\x1b[0;35m/home/flo/Untitled Document\x1b[0m\x1b[0;32m:\x1b[0m\x1b[0;32m2\x1b[0m\x1b[0;32m:\x1b[0maware werwer']
+    actual = print_helper.generate_output_for_matched_files_full_path(matched_items, search_term='aware',
+                                                                      is_from_stdin=False, is_line_by_line=True)
 
-	try:
-		assert actual == test_output
+    try:
+        assert actual == test_output
 
-	except AssertionError:
-		pytest.fail(actual)
+    except AssertionError:
+        pytest.fail(actual)
 
 
 def test_generate_output_for_matched_files_relative_path():
-	if platform.system() == 'Windows':
-		matched_items = {'./../../../../Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
+    if platform.system() == 'Windows':
+        matched_items = {
+            './../../../../Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
 
-	elif platform.system() == 'Linux':
-		matched_items = {'/home/flo/Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
+    elif platform.system() == 'Linux':
+        matched_items = {
+            '/home/flo/Untitled Document': {1: 'aware\n', 2: 'aware werwer\n'}}
 
-	else:
-		raise ValueError('No system information found.')
+    else:
+        raise ValueError('No system information found.')
 
-	test_output = [os.path.normpath('../../../../Untitled Document') + ':1:aware',
-	               os.path.normpath('../../../../Untitled Document') + ':2:aware werwer']
-	actual = print_helper.generate_output_for_matched_files_relative_path(matched_items, search_term='aware',
-	                                                                      is_from_stdin=False, is_line_by_line=True)
+    test_output = ['\x1b[0;35m../../../../Untitled Document\x1b[0m\x1b[0;34m:\x1b[0m\x1b[0;32m1\x1b[0m\x1b[0;34m:\x1b[0maware',
+                   '\x1b[0;35m../../../../Untitled Document\x1b[0m\x1b[0;34m:\x1b[0m\x1b[0;32m2\x1b[0m\x1b[0;34m:\x1b[0maware werwer']
+    actual = print_helper.generate_output_for_matched_files_relative_path(matched_items, search_term='aware',
+                                                                          is_from_stdin=False, is_line_by_line=True)
 
-	try:
-		assert actual == test_output
+    try:
+        assert actual == test_output
 
-	except AssertionError:
-		pytest.fail(actual)
+    except AssertionError:
+        pytest.fail(actual)
 
 
 def test_color_term_in_string():
-	test_list = ['Thebrownfoxjumpsover.']
-	term = 'fox'
-	# FYI \33 gets turned into \x1b
-	test_output = ['Thebrown\033[1;31mfox\033[0mjumpsover.']
+    test_list = ['Thebrownfoxjumpsover.']
+    term = 'fox'
+    # FYI \33 gets turned into \x1b
+    test_output = ['Thebrown\033[1;31mfox\033[0mjumpsover.']
 
-	actual = function_for_color_term_in_string(test_list, term)
+    actual = function_for_color_term_in_string(test_list, term)
 
-	assert actual == test_output
+    assert actual == test_output
 
 
 @print_helper.color_term_in_string
 def function_for_color_term_in_string(l, term):
-	return l
+    return l
 
 
 def test_outptut_binary_file_matches_full_path(with_f_bwrite):
-	matched_items = {with_f_bwrite.name: {'file_matched': ''}}
+    matched_items = {with_f_bwrite.name: {'file_matched': ''}}
 
-	test_output = ['Binary file ' + with_f_bwrite.name + ' matches']
-	actual = print_helper.generate_output_for_matched_files_full_path(matched_items, search_term='aware',
-	                                                                  is_from_stdin=False, is_line_by_line=False)
+    test_output = ['Binary file ' + with_f_bwrite.name + ' matches']
+    actual = print_helper.generate_output_for_matched_files_full_path(matched_items, search_term='aware',
+                                                                      is_from_stdin=False, is_line_by_line=False)
 
-	assert actual == test_output
+    assert actual == test_output
 
 
 def test_outptut_binary_file_matches_relative_path(with_f_bwrite):
-	matched_items = {with_f_bwrite.name: {'file_matched': ''}}
+    matched_items = {with_f_bwrite.name: {'file_matched': ''}}
 
-	test_output = ['Binary file ' + with_f_bwrite.name + ' matches']
-	actual = print_helper.generate_output_for_matched_files_relative_path(matched_items, search_term='aware',
-	                                                                      is_from_stdin=False, is_line_by_line=False)
+    test_output = ['Binary file ' + with_f_bwrite.name + ' matches']
+    actual = print_helper.generate_output_for_matched_files_relative_path(matched_items, search_term='aware',
+                                                                          is_from_stdin=False, is_line_by_line=False)
 
-	assert actual == test_output
+    assert actual == test_output
 
 
 def test_printing_for_searching_stdin_rel_path():
-	matched_items = {'/tmp/sadf.tmp': {'file': 'aware'}}
+    matched_items = {'/tmp/sadf.tmp': {'file': 'aware'}}
 
-	test_output = ['aware']
-	actual = print_helper.generate_output_for_matched_files_relative_path(matched_items, search_term='aware',
-	                                                                      is_from_stdin=True, is_line_by_line=False)
+    test_output = ['aware']
+    actual = print_helper.generate_output_for_matched_files_relative_path(matched_items, search_term='aware',
+                                                                          is_from_stdin=True, is_line_by_line=False)
 
-	assert actual == test_output
+    assert actual == test_output
 
 
 def test_printing_for_searching_stdin_abs_path():
-	matched_items = {'/tmp/sadf.tmp': {'file': 'aware'}}
+    matched_items = {'/tmp/sadf.tmp': {'file': 'aware'}}
 
-	test_output = ['aware']
-	actual = print_helper.generate_output_for_matched_files_full_path(matched_items, search_term='aware',
-	                                                                  is_from_stdin=True, is_line_by_line=False)
+    test_output = ['aware']
+    actual = print_helper.generate_output_for_matched_files_full_path(matched_items, search_term='aware',
+                                                                      is_from_stdin=True, is_line_by_line=False)
 
-	assert actual == test_output
+    assert actual == test_output
 
 
 # TODO do not call hotfix_delete_temp_dir manually
 def test_hotfix_delete_temp_dir(hotfix_delete_temp_dir):
-	pass
+    pass
